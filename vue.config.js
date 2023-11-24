7d7b25d997bd0c829de1ac1c0f61e3278e13e915
zhangjinxi <1334640772@qq.com>
2023-11-24 22:29:27 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ const port = process.env.port || process.env.npm_config_port || 9527 // dev port
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
   publicPath: '/',
-  outputDir: 'dist',
+  outputDir: 'dos',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
