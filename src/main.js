d9ea6646e1c9b8f610421512a3eb0234d2bb278b
zhangjinxi <1334640772@qq.com>
2023-11-28 00:12:19 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -4,12 +4,11 @@ import Cookies from 'js-cookie'
 // 微前端改造，注入新的publicPath
 import './utils/public-path'
 import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+import '@/styles/index.scss' // global css
 
 import Element from 'element-ui'
 import './styles/element-variables.scss'
 import enLang from 'element-ui/lib/locale/lang/en' // 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
-
-import '@/styles/index.scss' // global css
 
 import App from './App'
 import store from './store'
@@ -72,5 +71,5 @@ export async function unmount(props) {
   instance = null
   // 卸载路由和路由监听
   resetRouter()
-  router = null
+  // router = null
 }
